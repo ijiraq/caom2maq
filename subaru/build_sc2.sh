eefb82ed014b3812ff740e0f13c10a2fbd9cc1a2
JJ Kavelaars <jjk@uvic.ca>
2018-06-01 07:36:42 -0700

    run the caom2 ingest for a particular instrument setup

--- subaru/build_sc2.sh
+++ subaru/build_sc2.sh
@@ -1,4 +1,10 @@
-for year in 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017
-do 
-    caffeinate python focas2caom2.py FCS ${year} >& ${year}_err.txt &
-done 
+#!/bin/bash
+source activate astroconda
+# for camera in HSC FCS CIA
+for camera in SUP
+do
+   for year in 1999 2000 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017
+   do 
+      python subaru2caom2.py --repo tardis  --verbose ${camera} ${year} >& ${camera}_${year}_err.txt  & 
+   done 
+done
